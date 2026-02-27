04f8bc29a204d23322c050f33e48498748e4f1ad
unknown <zizifidele47@gmail.com>
2026-02-27 16:03:45 +0100

    header logic done

--- zando_front_angular/src/app/app.routes.ts
+++ zando_front_angular/src/app/app.routes.ts
@@ -4,6 +4,10 @@ export const routes: Routes = [
 
     {
         path:'',
+        loadComponent : () =>import('./../pages/home/home').then(a=> a.Home) 
+    },
+    {
+        path:'login',
         loadComponent : () =>import('./pages/login/login').then(a=> a.Login) 
     },
      
